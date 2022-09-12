553de66695cc8f38330accbb41e880208f000218
edwinsunny <106118110+edwinsunny@users.noreply.github.com>
2022-09-11 20:41:12 -0400

    new project update

---
+++ app/src/main/java/com/example/edwinsunny200515488/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.edwinsunny200515488;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
